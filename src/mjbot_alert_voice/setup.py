5a84805e58c111b95341a58423ce45144efb45ce
Topasm <dhkdwk0215@naver.com>
2023-09-18 14:17:12 +0900

    make package alert_voice

---
+++ src/mjbot_alert_voice/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'mjbot_alert_voice'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='drcl',
+    maintainer_email='example@naver.com',  # Updated email address
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "mjbot_alert_voice_comm = mjbot_alert_voice.main:main"
+        ],
+    },
+)
