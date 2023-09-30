221b7643a835605ef5d6bccd8f5dbeb1654961ca
Topasm <dhkdwk0215@naver.com>
2023-09-30 20:05:42 +0900

    making tracking node...

--- src/mjbot_vision/vision/main.py
+++ src/mjbot_vision/vision/main.py
@@ -3,8 +3,8 @@ import numpy as np
 import cv2
 from rknnlite.api import RKNNLite
 
-# from vision.depth import *
-# from .sort import Sort
+from vision.depth import *
+from vision.sort import Sort
 
 import random
 from itertools import count
@@ -14,27 +14,15 @@ from matplotlib.animation import FuncAnimation
 from random import randrange
 
 import csv
-from yolo import *
+import vision.yolo
 
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Bool
 from example_interfaces.msg import Int32
 from example_interfaces.msg import Int16MultiArray
-import threading
-import rclpy
-from rclpy.executors import MultiThreadedExecutor
 
-import os
-import urllib
-import traceback
-import time
-import sys
-
-import cv2
-from math import exp
-
-RKNN_MODEL = '/home/mju/Desktop/mjbot_2023/src/mjbot_vision/vision/yolov7-tiny_tk2_RK3588_i8.rknn'  # 절대경로
+RKNN_MODEL = '/home/drcl/ros2_ws/src/vision/vision/yolov5s-640-640-rk3588.rknn' # 절대경로
 
 
 class VisionNode(Node):
@@ -46,22 +34,18 @@ class VisionNode(Node):
         self.duration_sec = 0
         self.runtime_sec = 0
 
-        self.owner_size = [0, 0]  # [w, h]
-        self.owner_center = [0, 0]  # [cx, cy]
-        self.owner_size_prev = [0, 0]  # for fall detection
-        self.owner_size_diff = [0, 0]  # [w_diff, h_diff]
+        self.owner_size = [0 ,0] # [w, h]
+        self.owner_center = [0 ,0] # [cx, cy]
+        self.owner_size_prev = [0, 0] # for fall detection
+        self.owner_size_diff = [0, 0] # [w_diff, h_diff]
         self.owner_exists = False
         self.owner_fall = False
 
         self.publisher_fps_ = self.create_publisher(Int32, "fps", 10)
-        self.publisher_owner_exists_ = self.create_publisher(
-            Bool, "owner_exists", 10)
-        self.publisher_owner_center_ = self.create_publisher(
-            Int16MultiArray, "owner_center", 10)
-        self.publisher_owner_size_ = self.create_publisher(
-            Int16MultiArray, "owner_size", 10)
-        self.publisher_owner_fall_ = self.create_publisher(
-            Bool, "owner_fall", 10)
+        self.publisher_owner_exists_ = self.create_publisher(Bool, "owner_exists", 10)
+        self.publisher_owner_center_ = self.create_publisher(Int16MultiArray, "owner_center", 10)
+        self.publisher_owner_size_ = self.create_publisher(Int16MultiArray, "owner_size", 10)
+        self.publisher_owner_fall_ = self.create_publisher(Bool, "owner_fall", 10)
         self.get_logger().info("Node has been started")
 
     def publish_fps(self):
@@ -95,12 +79,8 @@ class VisionNode(Node):
         self.get_logger().info("PUB: /owner_fall: {}".format(msg.data))
 
     def init_video(self):
-        self.cap = cv2.VideoCapture(1)
+        self.cap = cv2.VideoCapture(0)
         # self.cap = cv2.VideoCapture('/home/drcl/ros2_ws/src/vision/vision/video/fall-01.mp4')
-
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
-
         if not self.cap.isOpened():
             self.get_logger().info("VIDEO: Cannot open video")
         else:
@@ -108,12 +88,8 @@ class VisionNode(Node):
 
     def read_video(self):
         ret, src = self.cap.read()
-        self.img = cv2.resize(src, dsize=(640, 360),)
-
-        self.img = cv2.copyMakeBorder(
-            self.img, 140, 140, 0, 0, cv2.BORDER_CONSTANT, (0, 0, 0))
-
-        return self.img
+        self.img = cv2.resize(src, dsize=(640, 360), interpolation=cv2.INTER_LINEAR)
+        self.img = cv2.copyMakeBorder(self.img, 140, 140, 0, 0, cv2.BORDER_CONSTANT, (0, 0, 0))
 
         # ret, src = self.cap.read()
         # self.img = cv2.resize(src, dsize=(0, 0), fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
@@ -139,38 +115,36 @@ class VisionNode(Node):
             exit(ret)
         self.get_logger().info('RKNN: Done')
 
-    def run_rknn(self, img):
+    def run_rknn(self):
         # Inference
         self.get_logger().info('RKNN: Inference')
-        outputs = self.rknn_lite.inference(inputs=[img])
+        outputs = self.rknn_lite.inference(inputs=[self.img])
         self.get_logger().info('RKNN: Done')
 
-        return outputs
+        # Post process
+        input0_data = outputs[0]
+        input1_data = outputs[1]
+        input2_data = outputs[2]
 
-        # # Post process
-        # input0_data = outputs[0]
-        # input1_data = outputs[1]
-        # input2_data = outputs[2]
+        input0_data = input0_data.reshape([3, -1]+list(input0_data.shape[-2:]))
+        input1_data = input1_data.reshape([3, -1]+list(input1_data.shape[-2:]))
+        input2_data = input2_data.reshape([3, -1]+list(input2_data.shape[-2:]))
 
-        # input0_data = input0_data.reshape([3, -1]+list(input0_data.shape[-2:]))
-        # input1_data = input1_data.reshape([3, -1]+list(input1_data.shape[-2:]))
-        # input2_data = input2_data.reshape([3, -1]+list(input2_data.shape[-2:]))
+        input_data = list()
+        input_data.append(np.transpose(input0_data, (2, 3, 0, 1)))
+        input_data.append(np.transpose(input1_data, (2, 3, 0, 1)))
+        input_data.append(np.transpose(input2_data, (2, 3, 0, 1)))
 
-        # input_data = list()
-        # input_data.append(np.transpose(input0_data, (2, 3, 0, 1)))
-        # input_data.append(np.transpose(input1_data, (2, 3, 0, 1)))
-        # input_data.append(np.transpose(input2_data, (2, 3, 0, 1)))
+        # Yolov5 post process
+        self.boxes, self.classes, self.scores = vision.yolo.yolov5_post_process(input_data)
 
-        # # Yolov5 post process
-        # self.boxes, self.classes, self.scores = vision.yolo.yolov5_post_process(input_data)
-
-        # # Draw detection box
-        # if self.boxes is not None:
-        #     self.owner_size, self.owner_center = vision.yolo.draw(self.img, self.boxes, self.scores, self.classes)
-        #     self.get_logger().info("Owner info (w: {}, h: {}, cx: {}, cy: {})".format(self.owner_size[0], self.owner_size[1], self.owner_center[0], self.owner_center[1]))
-        # else:
-        #     self.owner_size = [0, 0]
-        #     self.owner_center = [0, 0]
+        # Draw detection box
+        if self.boxes is not None:
+            self.owner_size, self.owner_center = vision.yolo.draw(self.img, self.boxes, self.scores, self.classes)
+            self.get_logger().info("Owner info (w: {}, h: {}, cx: {}, cy: {})".format(self.owner_size[0], self.owner_size[1], self.owner_center[0], self.owner_center[1]))
+        else:
+            self.owner_size = [0, 0]
+            self.owner_center = [0, 0]
 
     def init_sort(self):
         self.sort = Sort(max_age=2, min_hits=3, iou_threshold=0.3, )
@@ -182,37 +156,33 @@ class VisionNode(Node):
             tracker = self.sort.update(dets)
             for trk in tracker:
                 trk = trk.astype(int)
-                cv2.rectangle(
-                    self.img, (trk[0], trk[1]), (trk[2], trk[3]), (255, 0, 0), 2)
-                cv2.putText(
-                    self.img,  "ID:"+str(trk[4]), (trk[0], trk[1] + 12), 1, 1, (255, 255, 255), 2)
-
+                cv2.rectangle(self.img, (trk[0], trk[1]), (trk[2], trk[3]), (255, 0, 0), 2)
+                cv2.putText(self.img,  "ID:"+str(trk[4]), (trk[0], trk[1] + 12), 1, 1, (255, 255, 255), 2)
+            
     def detect_fall(self):
-        # if object detection is failed
-        if self.owner_size[0] == 0 and self.owner_size[1] == 0:
-            self.owner_size[0] = self.owner_size_prev[0]
-            self.owner_size[1] = self.owner_size_prev[1]
-            self.owner_exists = False
-        else:
-            self.owner_exists = True
+           # if object detection is failed
+            if self.owner_size[0] == 0 and self.owner_size[1] == 0:
+                self.owner_size[0] = self.owner_size_prev[0]
+                self.owner_size[1] = self.owner_size_prev[1]
+                self.owner_exists = False
+            else:
+                self.owner_exists = True
 
-        self.owner_size_diff[0] = (
-            self.owner_size[0] - self.owner_size_prev[0]) / self.duration_sec  # w_diff
-        self.owner_size_diff[1] = (
-            self.owner_size[1] - self.owner_size_prev[1]) / self.duration_sec  # h_diff
+            self.owner_size_diff[0] =  (self.owner_size[0] - self.owner_size_prev[0]) / self.duration_sec # w_diff
+            self.owner_size_diff[1] = (self.owner_size[1] - self.owner_size_prev[1]) / self.duration_sec # h_diff
 
-        if self.owner_size_diff[1] <= -500:
-            self.owner_fall = True
-        else:
-            self.owner_fall = False
+            if self.owner_size_diff[1] <= -500:
+                self.owner_fall = True
+            else:
+                self.owner_fall = False
 
-        self.owner_size_prev[0] = self.owner_size[0]
-        self.owner_size_prev[1] = self.owner_size[1]
+            self.owner_size_prev[0] = self.owner_size[0]
+            self.owner_size_prev[1] = self.owner_size[1] 
 
     def init_csv_log(self):
-        self.fieldnames = ["Duration", "Runtime", "FPS",
-                           "Width", "Height",
-                           "Width Diff", "Height Diff",
+        self.fieldnames = ["Duration", "Runtime", "FPS", 
+                           "Width", "Height", 
+                           "Width Diff", "Height Diff", 
                            "Owner Exists", "Fall Detection"]
 
         with open('/home/drcl/Desktop/mjbot_2023/src/mjbot_vision/vision/log.csv', 'w') as csv_file:
@@ -240,54 +210,44 @@ class VisionNode(Node):
 
             csv_writer.writerow(info)
 
-
 def main(args=None):
     rclpy.init(args=args)
     node = VisionNode()
 
-    anchors = np.array(values).reshape(3, -1, 2).tolist()
-
     node.init_video()
     node.init_rknn()
-
-    executor = MultiThreadedExecutor()
-    executor.add_node(node)
-    executor_thread = threading.Thread(target=executor.spin)
-    executor_thread.start()
-    # node.init_sort()
-    # node.init_csv_log()
+    node.init_sort()
+    node.init_csv_log()
 
     while True:
         start = dt.datetime.utcnow()
+        rclpy.spin_once(node, timeout_sec=0)
 
-        input_img = node.read_video()  # read and resize image
-        output_img = node.run_rknn(input_img)  # yolo inference
-        boxes, classes, scores = post_process(output_img, anchors)
-        result_img = draw(input_img, boxes, scores, classes)
-        # node.run_sort() # object tracking
+        node.read_video() # read and resize image
+        node.run_rknn() # yolo inference
+        node.run_sort() # object tracking
 
         duration = dt.datetime.utcnow() - start
         node.fps = int(round(1000000 / duration.microseconds))
 
-        # node.duration_sec = (duration.microseconds / 1000000)
-        # node.runtime_sec += node.duration_sec
-        # node.runtime_sec = round(node.runtime_sec, 3)
+        node.duration_sec = (duration.microseconds / 1000000)
+        node.runtime_sec += node.duration_sec
+        node.runtime_sec = round(node.runtime_sec, 3)
 
-        # node.run_csv_log()
-        # node.detect_fall()
-
-        # node.publish_fps()
-        # node.publish_owner_exists()
-        # node.publish_owner_size()
-        # node.publish_owner_center()
-        # if node.owner_fall:
-        #     node.publish_owner_fall()
+        node.run_csv_log()
+        node.detect_fall()
+    
+        node.publish_fps()
+        node.publish_owner_exists()
+        node.publish_owner_size()
+        node.publish_owner_center()
+        if node.owner_fall:
+            node.publish_owner_fall()
         # publish emotion
         # publish depth
 
-        cv2.putText(result_img, f'fps: {node.fps}',
-                    (25, 50), 1, 2, (0, 255, 0), 2)
-        cv2.imshow("result", result_img)
+        cv2.putText(node.img, f'fps: {node.fps}', (25, 50), 1, 2, (0, 255, 0), 2)
+        cv2.imshow("result", node.img)
 
         # If the `q` key was pressed, break from the loop
         key = cv2.waitKey(1) & 0xFF
--- src/mjbot_vision/vision/yolo.py
+++ src/mjbot_vision/vision/yolo.py
@@ -1,17 +1,10 @@
-import os
 import cv2
-import sys
-import argparse
-
-# from utils.coco_utils import COCO_test_helper
 import numpy as np
-values = [12.0, 16.0, 19.0, 36.0, 40.0, 28.0, 36.0, 75.0, 76.0,
-          55.0, 72.0, 146.0, 142.0, 110.0, 192.0, 243.0, 459.0, 401.0]
 
+OBJ_THRESH = 0.60
+NMS_THRESH = 0.60
+IMG_SIZE = 640
 
-IMG_SIZE = (640, 640)
-OBJ_THRESH = 0.25
-NMS_THRESH = 0.45
 CLASSES = ("person", "bicycle", "car", "motorbike ", "aeroplane ", "bus ", "train", "truck ", "boat", "traffic light",
            "fire hydrant", "stop sign ", "parking meter", "bench", "bird", "cat", "dog ", "horse ", "sheep", "cow", "elephant",
            "bear", "zebra ", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite",
@@ -20,35 +13,83 @@ CLASSES = ("person", "bicycle", "car", "motorbike ", "aeroplane ", "bus ", "trai
            "pottedplant", "bed", "diningtable", "toilet ", "tvmonitor", "laptop	", "mouse	", "remote ", "keyboard ", "cell phone", "microwave ",
            "oven ", "toaster", "sink", "refrigerator ", "book", "clock", "vase", "scissors ", "teddy bear ", "hair drier", "toothbrush ")
 
-coco_id_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34,
-                35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
-                64, 65, 67, 70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90]
-
-
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def xywh2xyxy(x):
+    # Convert [x, y, w, h] to [x1, y1, x2, y2]
+    y = np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+def process(input, mask, anchors):
+    anchors = [anchors[i] for i in mask]
+    grid_h, grid_w = map(int, input.shape[0:2])
+
+    box_confidence = sigmoid(input[..., 4])
+    box_confidence = np.expand_dims(box_confidence, axis=-1)
+
+    box_class_probs = sigmoid(input[..., 5:])
+
+    box_xy = sigmoid(input[..., :2])*2 - 0.5
+
+    col = np.tile(np.arange(0, grid_w), grid_w).reshape(-1, grid_w)
+    row = np.tile(np.arange(0, grid_h).reshape(-1, 1), grid_h)
+    col = col.reshape(grid_h, grid_w, 1, 1).repeat(3, axis=-2)
+    row = row.reshape(grid_h, grid_w, 1, 1).repeat(3, axis=-2)
+    grid = np.concatenate((col, row), axis=-1)
+    box_xy += grid
+    box_xy *= int(IMG_SIZE/grid_h)
+
+    box_wh = pow(sigmoid(input[..., 2:4])*2, 2)
+    box_wh = box_wh * anchors
+
+    box = np.concatenate((box_xy, box_wh), axis=-1)
+
+    return box, box_confidence, box_class_probs
 
 def filter_boxes(boxes, box_confidences, box_class_probs):
-    """Filter boxes with object threshold.
+    """Filter boxes with box threshold. It's a bit different with origin yolov5 post process!
+
+    # Arguments
+        boxes: ndarray, boxes of objects.
+        box_confidences: ndarray, confidences of objects.
+        box_class_probs: ndarray, class_probs of objects.
+
+    # Returns
+        boxes: ndarray, filtered boxes.
+        classes: ndarray, classes for boxes.
+        scores: ndarray, scores for boxes.
     """
+    boxes = boxes.reshape(-1, 4)
     box_confidences = box_confidences.reshape(-1)
-    candidate, class_num = box_class_probs.shape
+    box_class_probs = box_class_probs.reshape(-1, box_class_probs.shape[-1])
+
+    _box_pos = np.where(box_confidences >= OBJ_THRESH)
+    boxes = boxes[_box_pos]
+    box_confidences = box_confidences[_box_pos]
+    box_class_probs = box_class_probs[_box_pos]
 
     class_max_score = np.max(box_class_probs, axis=-1)
     classes = np.argmax(box_class_probs, axis=-1)
-
-    _class_pos = np.where(box_confidences >= OBJ_THRESH)
-    scores = (box_confidences)[_class_pos]
+    _class_pos = np.where(class_max_score >= OBJ_THRESH)
 
     boxes = boxes[_class_pos]
     classes = classes[_class_pos]
+    scores = (class_max_score* box_confidences)[_class_pos]
 
-    return boxes, classes, scores
-
+    return boxes, classes, scores    
 
 def nms_boxes(boxes, scores):
     """Suppress non-maximal boxes.
+
+    # Arguments
+        boxes: ndarray, boxes of objects.
+        scores: ndarray, scores of objects.
+
     # Returns
         keep: ndarray, index of effective boxes.
     """
@@ -78,97 +119,38 @@ def nms_boxes(boxes, scores):
         inds = np.where(ovr <= NMS_THRESH)[0]
         order = order[inds + 1]
     keep = np.array(keep)
-    return keep
+    return keep    
 
+def yolov5_post_process(input_data):
+    masks = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    anchors = [[10, 13], [16, 30], [33, 23], [30, 61], [62, 45],
+               [59, 119], [116, 90], [156, 198], [373, 326]]
 
-def dfl(position):
-    # Distribution Focal Loss (DFL)
-    import torch
-    x = torch.tensor(position)
-    n, c, h, w = x.shape
-    p_num = 4
-    mc = c//p_num
-    y = x.reshape(n, p_num, mc, h, w)
-    y = y.softmax(2)
-    acc_metrix = torch.tensor(range(mc)).float().reshape(1, 1, mc, 1, 1)
-    y = (y*acc_metrix).sum(2)
-    return y.numpy()
-
-
-def box_process(position, anchors):
-    grid_h, grid_w = position.shape[2:4]
-    col, row = np.meshgrid(np.arange(0, grid_w), np.arange(0, grid_h))
-    col = col.reshape(1, 1, grid_h, grid_w)
-    row = row.reshape(1, 1, grid_h, grid_w)
-    grid = np.concatenate((col, row), axis=1)
-    stride = np.array([IMG_SIZE[1]//grid_h, IMG_SIZE[0] //
-                      grid_w]).reshape(1, 2, 1, 1)
-
-    col = col.repeat(len(anchors), axis=0)
-    row = row.repeat(len(anchors), axis=0)
-    anchors = np.array(anchors)
-    anchors = anchors.reshape(*anchors.shape, 1, 1)
-
-    box_xy = position[:, :2, :, :]*2 - 0.5
-    box_wh = pow(position[:, 2:4, :, :]*2, 2) * anchors
-
-    box_xy += grid
-    box_xy *= stride
-    box = np.concatenate((box_xy, box_wh), axis=1)
-
-    # Convert [c_x, c_y, w, h] to [x1, y1, x2, y2]
-    xyxy = np.copy(box)
-    xyxy[:, 0, :, :] = box[:, 0, :, :] - box[:, 2, :, :] / 2  # top left x
-    xyxy[:, 1, :, :] = box[:, 1, :, :] - box[:, 3, :, :] / 2  # top left y
-    xyxy[:, 2, :, :] = box[:, 0, :, :] + \
-        box[:, 2, :, :] / 2  # bottom right x
-    xyxy[:, 3, :, :] = box[:, 1, :, :] + \
-        box[:, 3, :, :] / 2  # bottom right y
-
-    return xyxy
-
-
-def post_process(input_data, anchors):
-    boxes, scores, classes_conf = [], [], []
-
-    input_data = [_in.reshape(
-        [len(anchors[0]), -1]+list(_in.shape[-2:])) for _in in input_data]
-    for i in range(len(input_data)):
-        boxes.append(box_process(
-            input_data[i][:, :4, :, :], anchors[i]))
-        scores.append(input_data[i][:, 4:5, :, :])
-        classes_conf.append(input_data[i][:, 5:, :, :])
-
-    def sp_flatten(_in):
-        ch = _in.shape[1]
-        _in = _in.transpose(0, 2, 3, 1)
-        return _in.reshape(-1, ch)
-
-    boxes = [sp_flatten(_v) for _v in boxes]
-    classes_conf = [sp_flatten(_v) for _v in classes_conf]
-    scores = [sp_flatten(_v) for _v in scores]
+    boxes, classes, scores = [], [], []
+    for input, mask in zip(input_data, masks):
+        b, c, s = process(input, mask, anchors)
+        b, c, s = filter_boxes(b, c, s)
+        boxes.append(b)
+        classes.append(c)
+        scores.append(s)
 
     boxes = np.concatenate(boxes)
-    classes_conf = np.concatenate(classes_conf)
+    boxes = xywh2xyxy(boxes)
+    classes = np.concatenate(classes)
     scores = np.concatenate(scores)
 
-    # filter according to threshold
-    boxes, classes, scores = filter_boxes(boxes, scores, classes_conf)
-
-    # nms
     nboxes, nclasses, nscores = [], [], []
-
     for c in set(classes):
         inds = np.where(classes == c)
         b = boxes[inds]
         c = classes[inds]
         s = scores[inds]
+
         keep = nms_boxes(b, s)
 
-        if len(keep) != 0:
-            nboxes.append(b[keep])
-            nclasses.append(c[keep])
-            nscores.append(s[keep])
+        nboxes.append(b[keep])
+        nclasses.append(c[keep])
+        nscores.append(s[keep])
 
     if not nclasses and not nscores:
         return None, None, None
@@ -179,18 +161,45 @@ def post_process(input_data, anchors):
 
     return boxes, classes, scores
 
-
 def draw(image, boxes, scores, classes):
-    for box, score, cl in zip(boxes, scores, classes):
-        top, left, right, bottom = [int(_b) for _b in box]
-        print('class: {}, score: {}'.format(CLASSES[cl], score))
-        print('box coordinate left,top,right,down: [{}, {}, {}, {}]'.format(
-            top, left, right, bottom))
+    """Draw the boxes on the image.
+    # Argument:
+        image: original image.
+        boxes: ndarray, boxes of objects.
+        classes: ndarray, classes of objects.
+        scores: ndarray, scores of objects.
+        all_classes: all classes name.
+    """
+    p_size = [0, 0] # w, h
+    p_center = [0, 0] # cx, cy
 
-        cv2.rectangle(image, (top, left), (right, bottom), (255, 0, 0), 2)
-        cv2.putText(image, '{0} {1:.2f}'.format(CLASSES[cl], score),
-                    (top, left - 6),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.6, (0, 0, 255), 2)
-    return image
-#     return p_size, p_center
+    for box, score, cl in zip(boxes, scores, classes):
+        left, top, right, bottom = box
+        print('class: {}, score: {}'.format(CLASSES[cl], score))
+        print('box coordinate left,top,right,down: [{}, {}, {}, {}]'.format(top, left, right, bottom))
+        top = int(top)
+        left = int(left)
+        right = int(right)
+        bottom = int(bottom)
+
+        p_color = (0, 255, 0)
+        else_color = (255, 0, 0)
+
+        if CLASSES[cl] == 'person':
+            box_color = p_color
+
+            p_size[0] = (int)(right - left) # w
+            p_size[1] = (int)(bottom - top) # h
+
+            p_center[0] = (right + left) // 2 # cx
+            p_center[1] = (bottom + top) // 2 # cy
+
+            cv2.putText(image, 'w: {} h: {}'.format(p_size[0], p_size[1]), (p_center[0], top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, p_color, 2)
+            cv2.putText(image, 'cx: {} cy: {}'.format(p_center[0], p_center[1]), (p_center[0], p_center[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.6, p_color, 2)
+        else:
+            box_color = else_color
+
+        cv2.rectangle(image, (left, top), (right, bottom), box_color, 2)
+        cv2.putText(image, '{0} {1:.2f}'.format(CLASSES[cl], score), (left, top - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+    return p_size, p_center
