7de63e5e092bc97cf0e80a8eb505d717f4b20a23
Topasm <dhkdwk0215@naver.com>
2023-10-08 15:48:22 +0900

    disable arm

--- src/mjbot_control/launch/mjbot_control.launch.py
+++ src/mjbot_control/launch/mjbot_control.launch.py
@@ -7,23 +7,29 @@ from launch.substitutions import Command
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
+
 def generate_launch_description():
     # Define robot description using ros2 param
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    robot_description = Command(
+        ['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
     # Define paths to controller configuration files
     package_share_directory = get_package_share_directory("mjbot_control")
-    arm_controller_params_file = os.path.join(package_share_directory, 'config', 'arm_controllers.yaml')
-    diff_drive_controller_params_file = os.path.join(package_share_directory, 'config', 'diff_drive_controller.yaml')
-    arm_control_node = Node(package='mjbot_control',executable='arm_control_node.py',output='screen')
+    arm_controller_params_file = os.path.join(
+        package_share_directory, 'config', 'arm_controllers.yaml')
+    diff_drive_controller_params_file = os.path.join(
+        package_share_directory, 'config', 'diff_drive_controller.yaml')
+    arm_control_node = Node(package='mjbot_control',
+                            executable='arm_control_node.py', output='screen')
 
     # Node to run the controller manager
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[
-            {'robot_description': ParameterValue(robot_description, value_type=str)},
-            arm_controller_params_file,
+            {'robot_description': ParameterValue(
+                robot_description, value_type=str)},
+            # arm_controller_params_file,
             diff_drive_controller_params_file
         ],
         output="both",
@@ -33,16 +39,18 @@ def generate_launch_description():
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        arguments=["joint_state_broadcaster",
+                   "--controller-manager", "/controller_manager"],
     )
 
     # Node to spawn the diff drive controller
     diff_drive_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["diff_controller", "--controller-manager", "/controller_manager"],
+        arguments=["diff_controller",
+                   "--controller-manager", "/controller_manager"],
     )
-    
+
     # Node to spawn the joint trajectory controller
     load_trajectory_controller = Node(
         package="controller_manager",
@@ -55,7 +63,8 @@ def generate_launch_description():
     delay_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
         event_handler=OnProcessExit(
             target_action=joint_state_broadcaster_spawner,
-            on_exit=[diff_drive_controller_spawner, load_trajectory_controller],
+            on_exit=[diff_drive_controller_spawner,
+                     load_trajectory_controller],
         )
     )
 
