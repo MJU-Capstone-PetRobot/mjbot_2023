03594255b61c2ed254217daaf88c3023fc56602b
vellimole0621 <vellimole0621@gmail.com>
2023-10-10 16:03:41 +0900

    위험 감지 코드, 화재 추가

--- src/mjbot_alert/Alert/messageSending.py
+++ src/mjbot_alert/Alert/messageSending.py
@@ -1,22 +1,22 @@
 from twilio.rest import Client
-import serial
+# import serial
 import time
 
-def fire_check():
-    global fire
-    if __name__ == '__main__':
-        ser = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
-        ser.flush()
-
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            s_line = int(line)
-            if s_line > 200:
-                fire = 2
-            else:
-                fire = -1
-
-    return fire
+# def fire_check():
+#     global fire
+#     if __name__ == '__main__':
+#         ser = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
+#         ser.flush()
+#
+#         if ser.in_waiting > 0:
+#             line = ser.readline().decode('utf-8').rstrip()
+#             s_line = int(line)
+#             if s_line > 200:
+#                 fire = 2
+#             else:
+#                 fire = -1
+#
+#     return fire
 
 def googlemap_api():
     import json
