d8df2e7acca21c4c6d38a6e8da4e2316c2cca6e5
Topasm <dhkdwk0215@naver.com>
2023-10-16 20:01:55 +0900

    add neck movement at alert

--- src/mjbot_control/scripts/neck_controller.py
+++ src/mjbot_control/scripts/neck_controller.py
@@ -4,32 +4,31 @@ from std_msgs.msg import UInt16
 from rclpy.node import Node
 from example_interfaces.msg import Bool
 from example_interfaces.msg import Int32
-import numpy as np
 import time
 
 
 class NeckControllerPublisher(Node):
 
     def __init__(self):
-        super().__init__('float_publisher')
+        super().__init__('neck_controller_publisher')
         self.RPYpublisher = self.create_publisher(Vector3, 'float_values', 10)
         self.Zpublisher = self.create_publisher(UInt16, 'z_value', 10)
         self.dt = 0.5  # seconds
-        self.rostimer = 0.02
+        self.rostimer = 0.05
         self.timer = self.create_timer(self.rostimer, self.publish_values)
         self.originRPY = Vector3()
-        self.originRPY = [0, 0, 0]
         self.originZ = UInt16()
-        self.originZ = 70
-        self.targetRPY = Vector3()
-        self.targetRPY = [0, 0, 0]
-        self.targetZ = UInt16()
-        self.targetZ = 70
+        self.originRPY.x = 0.0
+        self.originRPY.y = 0.0
+        self.originRPY.z = 0.0
+        self.originZ.data = 70
         self.t0 = time.time()
+        self.targetRPY = Vector3()
+        self.targetZ = UInt16()
 
-        # 3차 경로 계획
-
-    def generate_trajectory(t0, tf, p0, pf):
+    def generate_trajectory(self, t0, tf, p0, pf):
+        p0 = float(p0)
+        pf = float(pf)
         tf_t0_3 = (tf - t0)**3
         a0 = pf*(t0**2)*(3*tf-t0) + p0*(tf**2)*(tf-3*t0)
         a0 = a0 / tf_t0_3
@@ -47,74 +46,76 @@ class NeckControllerPublisher(Node):
 
     def interpolate(self, r, p, y, z):
         a0t, a1t, a2t, a3t = self.generate_trajectory(
-            self.t0, self.t0+self.dt, self.originRPY.x, r)
+            0, self.dt, self.originRPY.x, r)
         a0x, a1x, a2x, a3x = self.generate_trajectory(
-            self.t0, self.t0+self.dt, self.originRPY.y, p)
+            0, self.dt, self.originRPY.y, p)
         a0y, a1y, a2y, a3y = self.generate_trajectory(
-            self.t0, self.t0+self.dt, self.originRPY.z, y)
+            0, self.dt, self.originRPY.z, y)
         a0z, a1z, a2z, a3z = self.generate_trajectory(
-            self.t0, self.t0+self.dt, self.originZ, z)
-        self.targetRPY.x = a0t + a1t * (self.t0-time.time()) + a2t * (
-            self.t0-time.time()) ** 2 + a3t * (self.t0-time.time()) ** 3
-        self.targetRPY.y = a0x + a1x * (self.t0-time.time()) + a2x * (
-            self.t0-time.time()) ** 2 + a3x * (self.t0-time.time()) ** 3
-        self.targetRPY.z = a0y + a1y * (self.t0-time.time()) + a2y * (
-            self.t0-time.time()) ** 2 + a3y * (self.t0-time.time()) ** 3
-        self.targetZ = a0z + a1z * (self.t0-time.time()) + a2z * \
-            (self.t0-time.time()) ** 2 + a3z * (self.t0-time.time()) ** 3
+            0, self.dt, self.originZ.data, z)
+        self.targetRPY.x = a0t + a1t * ((time.time()-self.t0)) + a2t * (
+            (time.time()-self.t0)) ** 2 + a3t * ((time.time()-self.t0)) ** 3
+        self.targetRPY.y = a0x + a1x * ((time.time()-self.t0)) + a2x * (
+            (time.time()-self.t0)) ** 2 + a3x * ((time.time()-self.t0)) ** 3
+        self.targetRPY.z = a0y + a1y * ((time.time()-self.t0)) + a2y * (
+            (time.time()-self.t0)) ** 2 + a3y * ((time.time()-self.t0)) ** 3
 
-        return
+        self.targetZ.data = round(a0z + a1z * ((time.time()-self.t0)) + a2z * (
+            (time.time()-self.t0)) ** 2 + a3z * ((time.time()-self.t0)) ** 3)
+        self.get_logger().info('self.targetRPY.x: %f' % self.targetRPY.x)
+        self.get_logger().info('self.targetRPY.y: %f' % self.targetRPY.y)
+        self.get_logger().info('self.targetRPY.z: %f' % self.targetRPY.z)
+        self.get_logger().info('self.targetZ.data: %f' % self.targetZ.data)
+
+        return time.time() - self.t0
 
     def publish_values(self, r, p, y, z):
-        self.interpolate(r, p, y, z)
-
         self.t0 = time.time()
-        msg = Vector3()
-        msg.x = self.targetRPY.x
-        msg.y = self.targetRPY.y
-        msg.z = self.targetRPY.z
-        zmsg = UInt16()
-        zmsg.data = self.targetZ
-        self.Zpublisher.publish(zmsg)
-        self.RPYpublisher.publish(msg)
-        self.get_logger().info('Published float values')
+        while time.time() - self.t0 < self.dt:
+            self.interpolate(r, p, y, z)
+            msg = Vector3()
+            msg.x = self.targetRPY.x
+            msg.y = self.targetRPY.y
+            msg.z = self.targetRPY.z
+            zmsg = UInt16()
+            zmsg.data = round(self.targetZ.data)
+            self.Zpublisher.publish(zmsg)
+            self.RPYpublisher.publish(msg)
+            time.sleep(self.rostimer)
+
+        self.get_logger().info('self.t0: %f' % self.t0)
 
 
-class alertSubscriber(Node):
+class AlertSubscriber(Node):
     def __init__(self):
         super().__init__('alert_subscriber')
-        self.subscription = self.create_subscription(
-            Bool, 'fall_down', self.subscribe_callback, 10)
-        self.subscription = self.create_subscription(
-            Int32, 'co_ppm', self.subscribe_callback_fire, 10)
+        self.fall_down_subscription = self.create_subscription(
+            Bool, 'fall_down', self.fall_down_callback, 10)
+        self.co_ppm_subscription = self.create_subscription(
+            Int32, 'co_ppm', self.co_ppm_callback, 10)
         self.neck_controller_publisher = NeckControllerPublisher()
 
-    def subscribe_callback(self, msg):
+    def fall_down_callback(self, msg):
+        if msg.data:
+            self.get_logger().info('Fall Down Alert')
+            self.neck_controller_publisher.publish_values(1, 1, 1, 100)
+            time.sleep(1)
+            self.neck_controller_publisher.publish_values(1, 1, 1, 60)
 
-        self.get_logger().info('Received: %d' % msg.data)
-
-        if msg.data == True:
-
-            self.neck_controller_publisher.publish_values(0, 0, 0, 100)
-            self.neck_controller_publisher.publish_values(0, 0, 0, 60)
-            self.get_logger().info('fall down')
-
-    def subscribe_callback_fire(self, msg):
-
-        self.get_logger().info('Received: %s' % msg.data)
-
+    def co_ppm_callback(self, msg):
         if msg.data >= 200:
-
-            self.neck_controller_publisher.publish_values(0, 0, 0, 100)
-            self.neck_controller_publisher.publish_values(0, 0, 0, 60)
-            self.get_logger().info('fall down')
+            self.get_logger().info('High CO PPM Alert')
+            self.neck_controller_publisher.publish_values(1, 1, 1, 100)
+            time.sleep(1)
+            self.neck_controller_publisher.publish_values(1, 1, 1, 60)
 
 
 def main(args=None):
     rclpy.init(args=args)
-    neck_controller_publisher = NeckControllerPublisher()
-    alert_subscriber = alertSubscriber()
-    rclpy.spin(neck_controller_publisher)
+    alert_subscriber = AlertSubscriber()
     rclpy.spin(alert_subscriber)
-    neck_controller_publisher.destroy_node()
-    alert_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
