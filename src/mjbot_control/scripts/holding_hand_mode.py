861646b81c9e9f5c29d33b941226f1a7bb4a1eed
Topasm <dhkdwk0215@naver.com>
2023-10-26 17:41:59 +0900

    add arm mode cancellation

--- src/mjbot_control/scripts/holding_hand_mode.py
+++ src/mjbot_control/scripts/holding_hand_mode.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Empty
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import JointState
 from std_msgs.msg import String
@@ -11,36 +10,23 @@ class HoldingHandNode(Node):
     def __init__(self):
         super().__init__('holding_hand_node')
 
-        # ... Define other properties like joint_efforts, poses, etc. ...
-
-        self.subscription_start = self.create_subscription(
-            Empty, 'start_holding_hand', self.holding_hand_callback, 10)
-        self.subscription_stop = self.create_subscription(
-            Empty, 'stop_holding_hand', self.stop_holding_hand_callback, 10)
+        self.subscription_mode = self.create_subscription(
+            String, 'mode', self.mode_callback, 10)
         self.pub_cmd_vel = self.create_publisher(Twist, 'cmd_vel', 10)
         self.joint_states_subscription = self.create_subscription(
             JointState, '/joint_states', self.joint_states_callback, 10)
-        self.joint_efforts = {}
 
-        self.should_continue = False
+        self.joint_efforts = {}
         self.target_cmd_vel = Twist()
         self.current_cmd_vel = Twist()
 
-    def control_callback(self, msg: String):
-        if msg.data == "start":
-            self.should_continue = True
+    def mode_callback(self, msg: String):
+        if msg.data == "holding_hand":
             self.holding_hand()
-        elif msg.data == "stop":
+        elif msg.data == "idle":
             self.should_continue = False
         else:
-            self.get_logger().warn("Unknown command received!")
-
-    def holding_hand_callback(self, msg):
-        self.should_continue = True
-        self.holding_hand()
-
-    def stop_holding_hand_callback(self, msg):
-        self.should_continue = False
+            self.get_logger().warn("Unknown mode received!")
 
     def joint_states_callback(self, msg: JointState):
         """Callback to handle incoming JointState messages."""
@@ -52,28 +38,34 @@ class HoldingHandNode(Node):
         return self.joint_efforts.get(joint_name, None)
 
     def holding_hand(self):
-        while not self.new_arm_mode_received:
 
-            r_shoulder_pitch_effort = self.get_joint_effort('r_shoulder_pitch')
-            r_shoulder_roll_effort = self.get_joint_effort('r_shoulder_roll')
-            self.get_logger().info('r_shoulder_pitch_effort: {0}'.format(
-                r_shoulder_pitch_effort))
-            self.get_logger().info('r_shoulder_roll_effort: {0}'.format(
-                r_shoulder_roll_effort))
+        r_shoulder_pitch_effort = self.get_joint_effort('r_shoulder_pitch')
+        r_shoulder_roll_effort = self.get_joint_effort('r_shoulder_roll')
 
-            cmd_vel = Twist()
+        cmd_vel = Twist()
 
-            # ... your logic for cmd_vel based on joint efforts ...
+        # Check r_shoulder_pitch effort and set linear.x
+        if r_shoulder_pitch_effort is not None:
+            if r_shoulder_pitch_effort > 220:
+                cmd_vel.linear.x = 0.5
+            elif r_shoulder_pitch_effort < -220:
+                cmd_vel.linear.x = -0.5
 
-            # Instead of immediately setting cmd_vel, set target_cmd_vel
-            self.target_cmd_vel = cmd_vel
-            # Call the ramp_cmd_vel method to update current_cmd_vel
-            self.ramp_cmd_vel()
+        # Check r_shoulder_roll effort and set angular.z and modify linear.x
+        if r_shoulder_roll_effort is not None:
+            if r_shoulder_roll_effort > 250:
+                cmd_vel.angular.z = 0.5
+                cmd_vel.linear.x = 0.1 if cmd_vel.linear.x == 0 else cmd_vel.linear.x
+            elif r_shoulder_roll_effort < -250:
+                cmd_vel.angular.z = -0.5
+                cmd_vel.linear.x = -0.1 if cmd_vel.linear.x == 0 else cmd_vel.linear.x
 
-            # Publish the ramped cmd_vel
-            self.move_base(self.current_cmd_vel.linear.x,
-                           self.current_cmd_vel.angular.z)
-        self.new_arm_mode_received = False
+        self.target_cmd_vel = cmd_vel
+        self.ramp_cmd_vel()
+
+        # Publish the cmd_vel
+        self.move_base(self.current_cmd_vel.linear.x,
+                       self.current_cmd_vel.angular.z)
 
     def ramp_cmd_vel(self, alpha=0.1):
         """
@@ -89,8 +81,6 @@ class HoldingHandNode(Node):
         cmd.linear.x = linear_x
         cmd.angular.z = angular_z
         self.pub_cmd_vel.publish(cmd)
-
-    # ... other required methods like set_and_send_arm_position, get_joint_effort ...
 
 
 if __name__ == '__main__':
--- src/mjbot_control/scripts/tracking_mode.py
+++ src/mjbot_control/scripts/tracking_mode.py
@@ -2,7 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Int16MultiArray
+from std_msgs.msg import Int16MultiArray, String
 
 
 class PIDController:
@@ -30,15 +30,23 @@ class Driver(Node):
             'owner_center',
             self.callback,
             10)
+        self.mode_subscription = self.create_subscription(
+            String,
+            'mode',
+            self.mode_callback,
+            10)
         self.base_cmd = Twist()
 
         self.target_distance = 800
-
-        # PID controller for yaw control
         self.yaw_pid = PIDController(0.5, 0.1, 0.1)
-
         self.image_width = 640
         self.image_height = 360
+
+    def mode_callback(self, msg: String):
+        if msg.data == "tracking":
+            self.current_mode = msg.data
+        else:
+            self.get_logger().warn(f"Unknown mode: {msg.data}")
 
     def update(self, linear_speed, angular_speed):
         self.base_cmd.linear.x = linear_speed
@@ -47,6 +55,10 @@ class Driver(Node):
         self.get_logger().info("Published: /cmd_vel_tracker: {}".format(self.base_cmd))
 
     def callback(self, msg):
+        if self.current_mode != "tracking":
+            return
+        if all(value == 0 for value in msg.data):
+            return
         person_x = msg.data[0] * 1.0
         person_y = msg.data[1] * 1.0
         person_distance = msg.data[2] * 1.0
