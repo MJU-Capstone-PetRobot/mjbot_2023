b97aff15716e13b1b31de8d2b8c26c8e6b1f8440
vellimole0621 <vellimole0621@gmail.com>
2023-11-08 18:38:46 +0900

    Feat gps 받는 node 생성

--- src/mjbot_alert/mjbot_alert.py
+++ src/mjbot_alert/mjbot_alert.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 import os
 import time
-
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Bool, Int32
+from std_msgs.msg import Bool, Int32, String
 from Alert.message_send import *
 
 
 class ListeningNode(Node):
     def __init__(self):
         super().__init__('Listening_node')
-
+        self.subscription = self.create_subscription(
+            String, 'gps', self.subscribe_callback_gps, 10)
         self.subscription = self.create_subscription(
             Int32, 'co_ppm', self.subscribe_callback_fire, 10)
-
 
 
     def subscribe_callback_fire(self, msg):
@@ -24,6 +23,39 @@ class ListeningNode(Node):
         if msg.data >= 200:
             send_message(2)  # 화재 사고 발생 문자 발송
             time.sleep(1000)
+
+    def subscribe_callback_gps(self, msg):
+        import json
+        self.get_logger().info('Received: %s' % msg.data)
+
+        data_ = list(msg.data)
+
+        lat = []
+        lon = []
+        j = 0
+        for i in range(0, len(data_)):
+            if data_[i] == ',':
+                j = i
+                break
+            else:
+                lat.append(data_[i])
+        for j in range(j, len(data_)):
+            if data_[j] == ' ':
+                pass
+            elif data_[j] == ",":
+                pass
+            else:
+                lon.append(data_[j])
+
+        latitude_str = "".join(lat)
+        longitude_str = "".join(lon)
+
+        write_data = {
+            "lat": f"{latitude_str}",
+            "lon": f"{longitude_str}"
+        }
+        with open('./user_gps.json', 'w') as d:
+            json.dump(write_data, d)
 
 
 def main(args=None):
