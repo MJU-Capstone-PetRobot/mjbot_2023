f88c37016da4a7de818967be8e017056f6fa5b5e
vellimole0621 <vellimole0621@gmail.com>
2023-11-08 18:41:07 +0900

    Refactor 필요없는 예전 gps 삭제

--- src/mjbot_alert/Alert/message_send.py
+++ src/mjbot_alert/Alert/message_send.py
@@ -1,27 +1,5 @@
-# import gps
-# import serial
-# import time
-# import pynmea2
 import googlemaps
 from twilio.rest import Client
-# from datetime import datetime
-
-
-# def gps_set():
-#     gpsd = gps.gps(mode=gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
-#     print("gps 작동 시작")
-#     while True:
-#         # Wait for valid data
-#         while True:
-#             nx = gpsd.next()
-#             if nx['class'] == 'TPV':
-#                 break
-#         # Extract gps info
-#         mju_latitude = getattr(nx, 'lat', "Unknown")
-#         mju_longitude = getattr(nx, 'lon', "Unknown")
-#         if mju_latitude != "Unknown" and mju_longitude != "Unknown":
-#             return [mju_latitude, mju_longitude]
-
 
 def googlemap_api():
     API = "AIzaSyD5fvkrnY2xbyp7DB9LK-bQbT1RzbgpvE8"  # API 값
@@ -60,34 +38,3 @@ def send_message(problem):
         to="+821066860215"
     )
     print("메세지 전송 완료")
-
-
-""" 여기 부분 예전 코드
-def gps_start():
-    필요한 gpsd 라이브러리 설치
-    sudo apt install gpsd,gpsd-clients
-    pip3 install gpsd-py3
-    pip3 install pynmea2
-
-    gps 사용 시작
-    sudo gpsd /dev/ttyFIQ0 -F /var/run/gpsd.sock
-    sudo chmod 666 /dev/ttyFIQ0     >> 권한 설정
-    sudo nano etcdefaultgpsd      >> gpsd 포트 지정
-    -> DEVICES=devttyFIQ0 -> CTRL+O -> ENTER -> CTRL+Y
-    sudo service gpsd restart
-    sudo service gpsd status     >> make sure active(running)
-    cgps
-
-    # 라즈베리파이 시리얼 통신
-    port = '/dev/ttyFIQ0'
-    baud = 9600
-    serialPort = serial.Serial(port, baudrate=baud, timeout=0.5)
-
-    #while True:
-    str = serialPort.readline().decode().strip()
-    if str.find('GGA') > 0:
-        msg = pynmea2.parse(str)
-        mj_latitude = round(msg.latitude, 6)
-        mj_longitude = round(msg.longitude, 6)
-    time.sleep(0.01)
-"""
