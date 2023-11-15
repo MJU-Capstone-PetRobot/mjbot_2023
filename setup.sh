e436e8eab2540eaa9bf41f4cfb88103c44efcee6
Kim Seong Hyeon <dhkdwk0215@naver.com>
2023-11-15 22:54:59 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,18 @@ sudo apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y
 
+# Install initial dependencies
 sudo apt-get install -y python3 python3-venv python3-pyaudio libportaudio2 libportaudiocpp0 portaudio19-dev libatlas-base-dev ffmpeg flac sense-hat
+
+# Install additional ROS 2 packages for Dynamixel, control, and xacro
+sudo apt-get install ros-humble-dynamixel-sdk
+sudo apt install ros-humble-dynamixel-workbench-toolbox
+sudo apt install ros-humble-ros2-control
+sudo apt install ros-humble-ros2-controllers
+sudo apt install ros-humble-xacro
+
+# Install more ROS 2 packages and dependencies
+sudo apt install ros-humble-joy-linux
+sudo apt install ros-humble-twist-mux
+sudo -H apt-get install -y ros-humble-joint-state-publisher
+sudo -H apt-get install -y python3-serial
