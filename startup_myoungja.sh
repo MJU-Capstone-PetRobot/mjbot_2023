370f534e07eff8a9f3923e0c721172ef7720485b
Topasm <dhkdwk0215@naver.com>
2023-11-28 17:22:18 +0900

    Update startup_myoungja.sh script

--- startup_myoungja.sh
+++ startup_myoungja.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
+sleep 2
 source /opt/ros/humble/setup.bash
 
 source /home/mju/mjbot_2023/install/setup.bash
 
-sleep 1
+sleep 2
 
 cd /home/mju/mjbot_2023/
 ros2 launch mjbot_bringup mjbot_bringup.launch.py
