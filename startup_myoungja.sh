12bf401e2126192fc9a17aac75af5b34d3ec48c1
Topasm <dhkdwk0215@naver.com>
2023-11-28 17:01:17 +0900

    Update startup script to use correct file paths

--- startup_myoungja.sh
+++ startup_myoungja.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 source /opt/ros/humble/setup.bash
 
-cd /home/mju/Desktop/mjbot_2023/
-colcon build
+source /home/mju/mjbot_2023/install/setup.bash
 
-source /home/mju/Desktop/mjbot_2023/install/setup.bash
+sleep 1
 
-sleep 2
-
-cd /home/mju/Desktop/mjbot_2023/
+cd /home/mju/mjbot_2023/
 ros2 launch mjbot_bringup mjbot_bringup.launch.py
